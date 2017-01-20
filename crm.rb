d189c6c7bae11fa819aee568c3b9fd5a90f4092b
Stephen H <stephen210huang@hotmail.com>
2017-01-20 14:09:26 -0500

    First commit

---
+++ crm.rb
@@ -0,0 +1,46 @@
+class CRM
+
+  def initialize(add, modify, delete, display_all, search_by_attr, exit)
+    @add = add
+    @modify = modify
+    @delete = delete
+    @display_all = display_all
+    @search_by_attr = search_by_attr
+    @exit = leave
+  end
+
+  def main_menu
+    puts "Would you like to add, modify, delete, display all, search by attribute, or exit?"
+      response = gets
+  end
+
+  def print_main_menu
+
+  end
+
+  def call_option
+
+  end
+
+  def add_new_contact
+
+  end
+
+  def modify_existing_contact
+
+  end
+
+  def delete_contact
+
+  end
+
+  def display_all_contacts
+
+  end
+
+  def search_by_attribute
+
+  end
+
+
+end
